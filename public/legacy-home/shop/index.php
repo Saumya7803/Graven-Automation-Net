9980ca2d573ed701f6163bb64fd3ea0db0b8638f
Saumya7803 <Saumya7803@users.noreply.github.com>
2026-04-23 13:21:32 +0530

    Replace About page with legacy site version

---
+++ public/legacy-home/shop/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="refresh" content="0; url=/shop" />
+    <title>Redirecting...</title>
+  </head>
+  <body>
+    <p><a href="/shop">Continue</a></p>
+  </body>
+  </html>
